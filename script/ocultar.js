b2d16983c9c836499a45b987735dfb7ee5742d49
MichaelSilva <michael.silva@manexware.com>
2019-09-11 14:40:04 -0500

    modificacion en el diseño, agregando funcionalidad en producto

--- script/ocultar.js
+++
@@ -1,9 +0,0 @@
-function ocultar(id){
-  if (document.getElementById){
-      var page = document.getElementById(id);
-      page.style.display = (page.style.display == 'none') ? 'block' : 'none';
-    }
-  }
-  window.onload = function(){
-  ocultar('contenido');
-}
---
+++ script/script.js
@@ -0,0 +1,24 @@
+function ocultar(id){
+  if (document.getElementById){
+      var page = document.getElementById(id);
+      page.style.display = (page.style.display == 'none') ? 'block' : 'none';
+    }
+  }
+  window.onload = function(){
+  ocultar('contenido');
+}
+
+$(function(){
+  var windowHeight = $(window).height();//tamaño de la ventana
+  var barraAltura = $('.barra').innerHeight();//altura de la barra
+  $(window).scroll(function(){
+      var scroll = $(window).scrollTop();
+      if (scroll > windowHeight){
+          $('.barra').addClass('fixed');
+          $('body').css({'margin-top': barraAltura+'px'});
+      }else{
+          $('.barra').removeClass('fixed');
+          $('body').css({'margin-top': '0px'});
+      }
+  });
+});
